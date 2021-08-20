2a62ca0f3c673349659caecbe2d12798d5d88f97
pranav <pranav_a@ece.iitr.ac.in>
2021-08-20 18:27:27 +0530

    Initial commit

---
+++ django_project/blog/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import post
+
+admin.site.register(post)
+# Register your models here.
+'''book an appointment
+filter for specalist 
+name of hospital 
+doctor
+
+book tests 
+
+awerness
+
+blogs notice
+
+record/summury 
+
+
+
+ambulance'''
