52f86c3915321b9f291bd22e88b913136f9bd993
pranav <pranav_a@ece.iitr.ac.in>
2021-09-05 09:50:00 +0530

    updated commit

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import profile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     class meta:
         model=User
         fields=["username","email","password1","password2"]
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class meta:
+        model=User
+        fields=["username","email"]
+class ProfileUpdateForm():
+    
+    class meta:
+        model=profile
+        fields=['image'] 
