1de8b1ef463e5c8042d002c97114954fd9ef15c4
pranav <pranav_a@ece.iitr.ac.in>
2021-09-09 15:52:04 +0530

    password rest email added

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.forms.widgets import EmailInput
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,11 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 
 LOGIN_REDIRECT_URL="blog-home"
 LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER= "*********"
+EMAIL_HOST_PASSWORD= "********"
